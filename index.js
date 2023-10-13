0961e8543e312e79a68cdcf9405867450f702f5f
ls1955 <xxlsxx1955@gmail.com>
2023-10-13 21:46:00 +0800

    Return data from getWeatherData
    Because:
    The return value will be use to show information
    in page

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function getWeatherData(location) {
     let key = "be3bc5872f9442e48b641132230810"
+    let result = null
 
     fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`)
         .then(response => response.json())
-        .then(console.log)
+        .then(response => result = response)
         .catch(console.log)
+
+    return result
 }
 
 getWeatherData("london")
