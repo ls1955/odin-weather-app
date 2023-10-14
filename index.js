3657e0ad36f660ae2205756093cf4a6bbf3e8a09
ls1955 <xxlsxx1955@gmail.com>
2023-10-14 21:48:59 +0800

    Rewrote getWeatherData
    This commit:
    * Return raw response upon successful request
    * Return the promise directly
    
    Because:
    * The process of the data should happen at elsewhere
      according to project outline
    * Previous statement that return result synchronously
      will return null, instead of later on successfully
      fetched data

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function getWeatherData(location) {
-    let key = "be3bc5872f9442e48b641132230810"
-    let result = null
+  let key = "be3bc5872f9442e48b641132230810";
 
-    fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`)
-        .then(response => response.json())
-        .then(response => result = response)
-        .catch(console.log)
+  return fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`)
+    .then((response) => {
+      if (!response.ok) throw new Error("Failed to fetch weather data.");
 
-    return result
+      return response;
+    })
+    .catch(console.log);
 }
 
-getWeatherData("london")
+getWeatherData("london");
